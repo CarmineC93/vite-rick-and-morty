60f8d80a372fac05fec94fd638e0aa90f0fcccbe
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-12 20:13:29 +0100

    better style

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -17,8 +17,8 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="grid">
+    <div class="container flex">
+        <div class="grid justify-between">
             <div class="row row-cols-5 m-2 g-3">
                 <div class="col" v-for="character in store.characters" :key="character.id">
                     <AppCard :character="character" />
@@ -30,5 +30,11 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    
+    .container{
+
+        background-image: linear-gradient(to left, 
+        rgba(82, 11, 154, 0.389), rgba(16, 170, 19, 0.389) , rgba(26, 131, 145, 0.389));
+
+        border-radius: 20px;
+    }
 </style>
